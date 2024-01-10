da12e7563580c227a013ff02d046873e2b261e09
Tejas Agarwal <102643335+tejas11111111@users.noreply.github.com>
2024-01-10 09:47:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import {fileURLToPath } from 'url';
 dotenv.config();
 connectDB();
 
-/esmodule fix
+//esmodule fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // rest objects
